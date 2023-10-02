927026792e604471aab268b87be268f46428e2dd
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-10-02 19:46:28 +0200

    Improve shortcut display strings

--- src/SimpleLevelEditor/Shortcut.cs
+++ src/SimpleLevelEditor/Shortcut.cs
@@ -5,6 +5,5 @@ namespace SimpleLevelEditor;
 
 public sealed record Shortcut(Keys Key, bool Ctrl, bool Shift, Action Action, string Description)
 {
-	// TODO: GetChar is not ideal for this.
-	public string KeyDescription { get; } = $"{(Ctrl ? "CTRL+" : string.Empty)}{(Shift ? "SHIFT+" : string.Empty)}{char.ToUpper(Key.GetChar(false) ?? '?')}";
+	public string KeyDescription { get; } = $"{(Ctrl ? "CTRL+" : string.Empty)}{(Shift ? "SHIFT+" : string.Empty)}{Key.GetDisplayString(true)}";
 }
