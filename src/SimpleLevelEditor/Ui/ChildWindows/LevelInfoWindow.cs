14c46aec20153ba6df54f4bc4d08665c25cbb65d
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-10-24 17:27:59 +0200

    Fix displaying (saved) incorrectly

--- src/SimpleLevelEditor/Ui/ChildWindows/LevelInfoWindow.cs
+++ src/SimpleLevelEditor/Ui/ChildWindows/LevelInfoWindow.cs
@@ -12,7 +12,7 @@ public static class LevelInfoWindow
 			ImGui.SeparatorText("Level Info");
 
 			ImGui.TextWrapped(LevelState.LevelFilePath ?? "<None>");
-			ImGui.Text(LevelState.IsModified ? "(unsaved changes)" : "(saved)");
+			ImGui.Text(LevelState.LevelFilePath == null ? string.Empty : LevelState.IsModified ? "(unsaved changes)" : "(saved)");
 			ImGui.Separator();
 			ImGui.Text(Inline.Span($"Version: {LevelState.Level.Version}"));
 			ImGui.Text(Inline.Span($"Meshes: {LevelState.Level.Meshes.Count}"));
