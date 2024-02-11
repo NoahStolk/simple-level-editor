207bb4feb73e48d57d48373b1bd27f5c39aaf2a3
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-02-11 15:52:08 +0100

    Change create and focus shortcuts to C and Q respectively

--- src/SimpleLevelEditor/Shortcuts.cs
+++ src/SimpleLevelEditor/Shortcuts.cs
@@ -23,9 +23,9 @@ public static class Shortcuts
 		new(Open, Keys.O, true, false, "Open level", LevelState.Load),
 		new(Save, Keys.S, true, false, "Save level", LevelState.Save),
 		new(SaveAs, Keys.S, true, true, "Save level as", LevelState.SaveAs),
-		new(AddNewObject, Keys.Space, false, false, "Add new object/entity", MainLogic.AddNew),
+		new(AddNewObject, Keys.C, false, false, "Add new object/entity", MainLogic.AddNew),
 		new(DeleteSelectedObjects, Keys.Delete, false, false, "Delete selected objects/entities", MainLogic.Remove),
-		new(FocusOnCurrentObject, Keys.A, false, false, "Focus on current object/entity", MainLogic.Focus),
+		new(FocusOnCurrentObject, Keys.Q, false, false, "Focus on current object/entity", MainLogic.Focus),
 		new(Undo, Keys.Z, true, false, "Undo", () => LevelState.SetHistoryIndex(LevelState.CurrentHistoryIndex - 1)),
 		new(Redo, Keys.Y, true, false, "Redo", () => LevelState.SetHistoryIndex(LevelState.CurrentHistoryIndex + 1)),
 	];
