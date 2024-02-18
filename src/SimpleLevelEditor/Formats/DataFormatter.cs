2a143d65924b085d59a41b30336bb1b2912cb335
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-02-18 14:36:29 +0100

    Add TryRead utility methods

--- src/SimpleLevelEditor/Formats/DataFormatter.cs
+++ src/SimpleLevelEditor/Formats/DataFormatter.cs
@@ -7,6 +7,91 @@ namespace SimpleLevelEditor.Formats;
 
 public static class DataFormatter
 {
+	public static bool TryReadBool(string? str, out bool result)
+	{
+		return bool.TryParse(str, out result);
+	}
+
+	public static bool TryReadInt(string? str, out int result)
+	{
+		return int.TryParse(str, NumberStyles.Integer, CultureInfo.InvariantCulture, out result);
+	}
+
+	public static bool TryReadFloat(string? str, out float result)
+	{
+		return float.TryParse(str, NumberStyles.Float, CultureInfo.InvariantCulture, out result);
+	}
+
+	public static bool TryReadVector2(string? str, out Vector2 result)
+	{
+		result = default;
+
+		if (str == null)
+			return false;
+
+		string[] parts = str.Split(' ');
+		if (parts.Length != 2)
+			return false;
+
+		return float.TryParse(parts[0], NumberStyles.Float, CultureInfo.InvariantCulture, out result.X) && float.TryParse(parts[1], NumberStyles.Float, CultureInfo.InvariantCulture, out result.Y);
+	}
+
+	public static bool TryReadVector3(string? str, out Vector3 result)
+	{
+		result = default;
+
+		if (str == null)
+			return false;
+
+		string[] parts = str.Split(' ');
+		if (parts.Length != 3)
+			return false;
+
+		return float.TryParse(parts[0], NumberStyles.Float, CultureInfo.InvariantCulture, out result.X) && float.TryParse(parts[1], NumberStyles.Float, CultureInfo.InvariantCulture, out result.Y) && float.TryParse(parts[2], NumberStyles.Float, CultureInfo.InvariantCulture, out result.Z);
+	}
+
+	public static bool TryReadVector4(string? str, out Vector4 result)
+	{
+		result = default;
+
+		if (str == null)
+			return false;
+
+		string[] parts = str.Split(' ');
+		if (parts.Length != 4)
+			return false;
+
+		return float.TryParse(parts[0], NumberStyles.Float, CultureInfo.InvariantCulture, out result.X) && float.TryParse(parts[1], NumberStyles.Float, CultureInfo.InvariantCulture, out result.Y) && float.TryParse(parts[2], NumberStyles.Float, CultureInfo.InvariantCulture, out result.Z) && float.TryParse(parts[3], NumberStyles.Float, CultureInfo.InvariantCulture, out result.W);
+	}
+
+	public static bool TryReadRgb(string? str, out Rgb result)
+	{
+		result = default;
+
+		if (str == null)
+			return false;
+
+		string[] parts = str.Split(' ');
+		if (parts.Length != 3)
+			return false;
+
+		return byte.TryParse(parts[0], NumberStyles.Integer, CultureInfo.InvariantCulture, out result.R) && byte.TryParse(parts[1], NumberStyles.Integer, CultureInfo.InvariantCulture, out result.G) && byte.TryParse(parts[2], NumberStyles.Integer, CultureInfo.InvariantCulture, out result.B);
+	}
+
+	public static bool TryReadRgba(string? str, out Rgba result)
+	{
+		result = default;
+
+		if (str == null)
+			return false;
+
+		string[] parts = str.Split(' ');
+		if (parts.Length != 4)
+			return false;
+
+		return byte.TryParse(parts[0], NumberStyles.Integer, CultureInfo.InvariantCulture, out result.R) && byte.TryParse(parts[1], NumberStyles.Integer, CultureInfo.InvariantCulture, out result.G) && byte.TryParse(parts[2], NumberStyles.Integer, CultureInfo.InvariantCulture, out result.B) && byte.TryParse(parts[3], NumberStyles.Integer, CultureInfo.InvariantCulture, out result.A);
+	}
+
 	public static bool ReadBool(string str)
 	{
 		return bool.Parse(str);
