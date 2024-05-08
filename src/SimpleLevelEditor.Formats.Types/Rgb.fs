780ece8c8d999d60391a36c966a331d72f6fa246
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-05-08 18:27:39 +0200

    Add ToVector methods to color types

--- src/SimpleLevelEditor.Formats.Types/Rgb.fs
+++ src/SimpleLevelEditor.Formats.Types/Rgb.fs
@@ -1,7 +1,6 @@
 namespace SimpleLevelEditor.Formats.Types
 
-open System
-open System.Globalization
+open System.Numerics
 
 [<Struct>]
 type Rgb =
@@ -11,6 +10,10 @@ type Rgb =
         val mutable B: byte
 
         new(r, g, b) = { R = r; G = g; B = b }
+
+        member this.ToVector3() = Vector3(float32 this.R / 255.0f, float32 this.G / 255.0f, float32 this.B / 255.0f)
+
+        member this.ToVector4() = Vector4(float32 this.R / 255.0f, float32 this.G / 255.0f, float32 this.B / 255.0f, 1.0f)
 
         static member Default = Rgb(0uy, 0uy, 0uy)
     end
