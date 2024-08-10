ea76572dbf4c81e7271ea6052c3c4e2d05aae139
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-08-10 21:47:20 +0200

    Improve entity descriptor popup UI

--- src/GameEntityConfig.Editor/Ui/GameEntityConfigBuilder/CreateNewEntityDescriptorPopup.cs
+++ src/GameEntityConfig.Editor/Ui/GameEntityConfigBuilder/CreateNewEntityDescriptorPopup.cs
@@ -27,78 +27,8 @@ public sealed class CreateNewEntityDescriptorPopup
 		{
 			ImGui.InputText("Name", ref _name, 100);
 
-			ImGui.SeparatorText("Fixed Components");
-
-			if (ImGui.BeginTable("FixedComponentsTable", 3, ImGuiTableFlags.Borders))
-			{
-				ImGui.TableSetupColumn("Remove", ImGuiTableColumnFlags.WidthFixed, 64);
-				ImGui.TableSetupColumn("Type", ImGuiTableColumnFlags.WidthFixed, 128);
-				ImGui.TableSetupColumn("Value", ImGuiTableColumnFlags.WidthStretch);
-
-				ImGui.TableSetupScrollFreeze(0, 1);
-				ImGui.TableHeadersRow();
-
-				for (int i = 0; i < _fixedComponents.Count; i++)
-				{
-					FixedComponent fixedComponent = _fixedComponents[i];
-
-					ImGui.TableNextRow();
-
-					ImGui.TableNextColumn();
-					if (ImGui.Button(Inline.Span($"X##{i}")))
-						_fixedComponents.RemoveAt(i);
-					if (ImGui.IsItemHovered())
-						ImGui.SetTooltip("Remove this component");
-
-					ImGui.TableNextColumn();
-					if (ImGui.BeginCombo(Inline.Span($"##Type{i}"), fixedComponent.Type?.Name ?? "<NONE>", ImGuiComboFlags.HeightLarge))
-					{
-						List<TypeInfo> allComponentTypes = state.ComponentTypes;
-						if (state.EnableDefaultComponents)
-							allComponentTypes = allComponentTypes.Concat(ComponentUtils.DefaultComponents).ToList();
-
-						foreach (TypeInfo type in allComponentTypes)
-						{
-							if (ImGui.Selectable(type.Name))
-								_fixedComponents[i].Type = type;
-						}
-
-						ImGui.EndCombo();
-					}
-
-					ImGui.TableNextColumn();
-					string temp = fixedComponent.Value;
-					if (ImGui.InputText(Inline.Span($"##Value{i}"), ref temp, 100))
-						_fixedComponents[i].Value = temp;
-				}
-
-				ImGui.EndTable();
-			}
-
-			if (ImGui.Button("+##Fixed", new Vector2(32, 32)))
-				_fixedComponents.Add(new FixedComponent());
-
-			ImGui.SeparatorText("Varying Components");
-
-			for (int j = _varyingComponents.Count - 1; j >= 0; j--)
-			{
-				VaryingComponent varyingComponent = _varyingComponents[j];
-				string varyingComponentTypeName = varyingComponent.Type?.Name ?? "<NONE>";
-				ImGui.Button($"X##{varyingComponentTypeName}");
-				ImGui.SameLine();
-				ImGui.Text(varyingComponentTypeName);
-				ImGui.SameLine();
-				ImGui.InputText("Default Value", ref varyingComponent.DefaultValue, 100);
-				ImGui.SameLine();
-				ImGui.InputText("Step Value", ref varyingComponent.StepValue, 100);
-				ImGui.SameLine();
-				ImGui.InputText("Min Value", ref varyingComponent.MinValue, 100);
-				ImGui.SameLine();
-				ImGui.InputText("Max Value", ref varyingComponent.MaxValue, 100);
-			}
-
-			if (ImGui.Button("+##Varying", new Vector2(32, 32)))
-				_varyingComponents.Add(new VaryingComponent());
+			RenderFixedComponents(state);
+			RenderVaryingComponents(state);
 		}
 
 		ImGui.EndChild();
@@ -128,6 +58,132 @@ public sealed class CreateNewEntityDescriptorPopup
 		ImGui.EndChild();
 
 		return null;
+	}
+
+	private void RenderFixedComponents(GameEntityConfigBuilderState state)
+	{
+		ImGui.SeparatorText("Fixed Components");
+
+		if (ImGui.BeginTable("FixedComponentsTable", 3, ImGuiTableFlags.Borders))
+		{
+			ImGui.TableSetupColumn("Remove", ImGuiTableColumnFlags.WidthFixed, 64);
+			ImGui.TableSetupColumn("Type", ImGuiTableColumnFlags.WidthFixed, 128);
+			ImGui.TableSetupColumn("Value", ImGuiTableColumnFlags.WidthStretch);
+
+			ImGui.TableSetupScrollFreeze(0, 1);
+			ImGui.TableHeadersRow();
+
+			for (int i = 0; i < _fixedComponents.Count; i++)
+			{
+				FixedComponent fixedComponent = _fixedComponents[i];
+
+				ImGui.TableNextRow();
+
+				ImGui.TableNextColumn();
+				if (ImGui.Button(Inline.Span($"X##Fixed{i}")))
+					_fixedComponents.RemoveAt(i);
+				if (ImGui.IsItemHovered())
+					ImGui.SetTooltip("Remove this component");
+
+				ImGui.TableNextColumn();
+				if (ImGui.BeginCombo(Inline.Span($"##FixedType{i}"), fixedComponent.Type?.Name ?? "<NONE>", ImGuiComboFlags.HeightLarge))
+				{
+					List<TypeInfo> allComponentTypes = state.ComponentTypes;
+					if (state.EnableDefaultComponents)
+						allComponentTypes = allComponentTypes.Concat(ComponentUtils.DefaultComponents).ToList();
+
+					foreach (TypeInfo type in allComponentTypes)
+					{
+						if (ImGui.Selectable(type.Name))
+							_fixedComponents[i].Type = type;
+					}
+
+					ImGui.EndCombo();
+				}
+
+				ImGui.TableNextColumn();
+				string temp = fixedComponent.Value;
+				if (ImGui.InputText(Inline.Span($"##Value{i}"), ref temp, 100))
+					_fixedComponents[i].Value = temp;
+			}
+
+			ImGui.EndTable();
+		}
+
+		if (ImGui.Button("+##Fixed", new Vector2(32, 32)))
+			_fixedComponents.Add(new FixedComponent());
+	}
+
+	private void RenderVaryingComponents(GameEntityConfigBuilderState state)
+	{
+		ImGui.SeparatorText("Varying Components");
+
+		if (ImGui.BeginTable("VaryingComponentsTable", 6, ImGuiTableFlags.Borders))
+		{
+			ImGui.TableSetupColumn("Remove", ImGuiTableColumnFlags.WidthFixed, 64);
+			ImGui.TableSetupColumn("Type", ImGuiTableColumnFlags.WidthFixed, 128);
+			ImGui.TableSetupColumn("Default", ImGuiTableColumnFlags.WidthFixed, 128);
+			ImGui.TableSetupColumn("Step", ImGuiTableColumnFlags.WidthFixed, 128);
+			ImGui.TableSetupColumn("Min", ImGuiTableColumnFlags.WidthFixed, 128);
+			ImGui.TableSetupColumn("Max", ImGuiTableColumnFlags.WidthFixed, 128);
+
+			ImGui.TableSetupScrollFreeze(0, 1);
+			ImGui.TableHeadersRow();
+
+			for (int i = 0; i < _varyingComponents.Count; i++)
+			{
+				VaryingComponent varyingComponent = _varyingComponents[i];
+
+				ImGui.TableNextRow();
+
+				ImGui.TableNextColumn();
+				if (ImGui.Button(Inline.Span($"X##Varying{i}")))
+					_varyingComponents.RemoveAt(i);
+				if (ImGui.IsItemHovered())
+					ImGui.SetTooltip("Remove this component");
+
+				ImGui.TableNextColumn();
+				if (ImGui.BeginCombo(Inline.Span($"##VaryingType{i}"), varyingComponent.Type?.Name ?? "<NONE>", ImGuiComboFlags.HeightLarge))
+				{
+					List<TypeInfo> allComponentTypes = state.ComponentTypes;
+					if (state.EnableDefaultComponents)
+						allComponentTypes = allComponentTypes.Concat(ComponentUtils.DefaultComponents).ToList();
+
+					foreach (TypeInfo type in allComponentTypes)
+					{
+						if (ImGui.Selectable(type.Name))
+							_varyingComponents[i].Type = type;
+					}
+
+					ImGui.EndCombo();
+				}
+
+				ImGui.TableNextColumn();
+				string temp = varyingComponent.DefaultValue;
+				if (ImGui.InputText(Inline.Span($"##DefaultValue{i}"), ref temp, 100))
+					_varyingComponents[i].DefaultValue = temp;
+
+				ImGui.TableNextColumn();
+				temp = varyingComponent.StepValue;
+				if (ImGui.InputText(Inline.Span($"##StepValue{i}"), ref temp, 100))
+					_varyingComponents[i].StepValue = temp;
+
+				ImGui.TableNextColumn();
+				temp = varyingComponent.MinValue;
+				if (ImGui.InputText(Inline.Span($"##MinValue{i}"), ref temp, 100))
+					_varyingComponents[i].MinValue = temp;
+
+				ImGui.TableNextColumn();
+				temp = varyingComponent.MaxValue;
+				if (ImGui.InputText(Inline.Span($"##MaxValue{i}"), ref temp, 100))
+					_varyingComponents[i].MaxValue = temp;
+			}
+
+			ImGui.EndTable();
+		}
+
+		if (ImGui.Button("+##Varying", new Vector2(32, 32)))
+			_varyingComponents.Add(new VaryingComponent());
 	}
 
 	private EntityDescriptor ConstructEntityDescriptor()
